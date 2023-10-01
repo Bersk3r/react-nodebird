ad0a735596791b2d0f6069de27cfc9360836d03f
Bersk3r <kiscwook@gmail.com>
2023-10-02 03:19:44 +0900

    middle save

--- c2/front/pages/profile.js
+++ c2/front/pages/profile.js
@@ -1,15 +1,21 @@
 // import React from 'react';
 import Head from 'next/head';
 import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
 
 const Profile = () => {
+    const followingList = [{ nickname: '배재민'}, {nickname: '강성호'}, {nickname: '박병호'}];
+    const followerList = [{ nickname: '배재민'}, {nickname: '강성호'}, {nickname: '박병호'}];
     return (
         <>
             <Head>
                 <title>내 프로필 | NodeBird</title>
             </Head>
             <AppLayout>
-                <div>내 프로필</div>
+                <NicknameEditForm />
+                <FollowList header="팔로잉 목록" data={followingList} />
+                <FollowList header="팔로워 목록" data={followerList} />
             </AppLayout>
         </>
     );
