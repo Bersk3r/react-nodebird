324744b5a30b7b0ce7394a5f14a209928f290d05
Bersk3r <kiscwook@gmail.com>
2023-10-02 01:26:33 +0900

    middle save

--- c2/front/components/AppLayout.js
+++ c2/front/components/AppLayout.js
@@ -1,9 +1,18 @@
-// import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from "prop-types";
 import Link from 'next/link';
 import { Menu, Input, Row, Col } from 'antd';
+import styled from 'styled-components';
+import UserProfile from '../components/UserProfile';
+import LoginForm from '../components/LoginForm';
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
 
 const AppLayout = ({children}) => {
+    const [ isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
         <div>
             <Menu mode="horizontal">
@@ -14,7 +23,7 @@ const AppLayout = ({children}) => {
                     <Link href="/profile"><a>프로필</a></Link>
                 </Menu.Item>
                 <Menu.Item>
-                    <Input.Search enterButton style={{verticalAlign: 'middle'}}/>
+                    <SearchInput enterButton  />
                 </Menu.Item>
                 <Menu.Item>
                     <Link href="/signup"><a>회원가입</a></Link>
@@ -22,7 +31,7 @@ const AppLayout = ({children}) => {
             </Menu>
             <Row gutter={8}>
                 <Col xs={24} md={6}>
-                    왼쪽 메뉴
+                    {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn} /> : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
                 </Col>
                 <Col xs={24} md={12}>
                     {children}
