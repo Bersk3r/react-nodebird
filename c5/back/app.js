4f6d4af79ff5d10024765d1bee4da63b18e30596
Bersk3r <kiscwook@gmail.com>
2023-10-24 19:50:48 +0900

    middle save

--- c5/back/app.js
+++ c5/back/app.js
@@ -2,7 +2,14 @@
 const express = require('express');
 const postRouter = require('./routes/post');
 
+const db = require('./models');
 const app = express();
+
+db.sequelize.sync()
+    .then( () => {
+        console.log('db 연결 성공');
+    })
+    .catch(console.error);
 
 app.get('/',(req, res) => {
     res.send('hello express');
@@ -19,7 +26,7 @@ app.use('/post',postRouter);
 
 
 app.listen(3065, () => {
-    console.log('서버 실행 중');
+    console.log('서버 실행 중!');
 });
 
 /*
