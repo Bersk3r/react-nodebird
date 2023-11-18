36415919c4c3c3e6d81e4abd1b6189f4ad1bd1f3
Bersk3r <kiscwook@gmail.com>
2023-11-19 04:27:12 +0900

    react-nodebird ended!

--- c7/back/routes/posts.js
+++ c7/back/routes/posts.js
@@ -54,6 +54,124 @@ router.get('/', async (req, res, next) => { // GET /posts
     }
 })
 
+router.get('/related', async (req, res, next) => { // GET /posts
+    try {
+        const followings = await User.findAll({
+            attributes: ['id'],
+            include: [{
+                mode: User,
+                as: 'Followers',
+                where: { id: req.user.id }
+            }]
+        });
+        const where = {
+            UserId: { [Op.in]: followings.map((v) => v.id) }
+        };
+        if (parseInt(req.query.lastId,10)) { // 초기 로딩이 아닐 때
+            where.id = { [Op.lt] : parseInt(req.query.lastId, 10) }
+        }
+        const posts = await Post.findAll({
+            where,
+            // where: { id: lastId },
+            limit: 10,
+            // offset: 0, // 추가 및 삭제 시 오류가 많아, lastId를 많이 사용함
+            order: [
+                ['createdAt', 'DESC'],
+                [Comment, 'createdAt', 'DESC'],
+            ],
+            include: [{
+                model: User,
+                attributes: ['id', 'nickname'],
+            }, {
+                model: Image,
+            }, {
+                model: Comment,
+                include: [{
+                    model: User,
+                    attributes: ['id', 'nickname'],
+                }]
+            }, {
+                model: User, // 좋아요 누른 사람
+                as: 'Likers',
+                attributes: [ 'id' ],
+            },{
+                model: Post,
+                as: 'Retweet',
+                include: [{
+                    model: User,
+                    attributes: ['id', 'nickname'],
+                }, {
+                    model: Image,
+                }]
+            }],
+        });
+        // console.log(posts);
+        res.status(200).json(posts);
+    } catch(error) {
+        console.error(error);
+        next(error);
+    }
+})
+
+router.get('/unrelated', async (req, res, next) => { // GET /posts
+    try {
+        const followings = await User.findAll({
+            attributes: ['id'],
+            include: [{
+                mode: User,
+                as: 'Followers',
+                where: { id: req.user.id }
+            }]
+        });
+        const where = {
+            UserId: { [Op.notIn]: followings.map((v) => v.id).concat(req.user.id) }
+        };
+        if (parseInt(req.query.lastId,10)) { // 초기 로딩이 아닐 때
+            where.id = { [Op.lt] : parseInt(req.query.lastId, 10) }
+        }W
+        const posts = await Post.findAll({
+            where,
+            // where: { id: lastId },
+            limit: 10,
+            // offset: 0, // 추가 및 삭제 시 오류가 많아, lastId를 많이 사용함
+            order: [
+                ['createdAt', 'DESC'],
+                [Comment, 'createdAt', 'DESC'],
+            ],
+            include: [{
+                model: User,
+                attributes: ['id', 'nickname'],
+            }, {
+                model: Image,
+            }, {
+                model: Comment,
+                include: [{
+                    model: User,
+                    attributes: ['id', 'nickname'],
+                }]
+            }, {
+                model: User, // 좋아요 누른 사람
+                as: 'Likers',
+                attributes: [ 'id' ],
+            },{
+                model: Post,
+                as: 'Retweet',
+                include: [{
+                    model: User,
+                    attributes: ['id', 'nickname'],
+                }, {
+                    model: Image,
+                }]
+            }],
+        });
+        // console.log(posts);
+        res.status(200).json(posts);
+    } catch(error) {
+        console.error(error);
+        next(error);
+    }
+})
+
 // pagenation : 무한 스크롤링 & 게시판
 
 module.exports = router;
